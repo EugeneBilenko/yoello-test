975a5c309093d4b11d56f20b4de214b5576adb7a
EugeneBilenko <eugen.bilenko@gmail.com>
2020-05-12 20:02:22 +0300

    Updates

---
+++ precache-manifest.135c32d1c7c34d1586888a578a20f76b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5785e7a872e4c08f4c4007564ee3f141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9946a2ea9f6b4cd4dd33",
+    "url": "/static/css/main.04b2a7a7.chunk.css"
+  },
+  {
+    "revision": "332410a19e14a24b600d",
+    "url": "/static/js/2.5cd98b23.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.5cd98b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9946a2ea9f6b4cd4dd33",
+    "url": "/static/js/main.c35b8b28.chunk.js"
+  },
+  {
+    "revision": "54a7d1d5ddab00fa22cb",
+    "url": "/static/js/runtime-main.6d6d43ce.js"
+  }
+]);
